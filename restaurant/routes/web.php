ef6039ff7bbeeac2c81a85e804bf823c1245af78
Antonis Zalonis <antonis.zalonis@gmail.com>
2023-02-08 01:09:21 +0100

    Added security in the links

--- restaurant/routes/web.php
+++ restaurant/routes/web.php
@@ -21,5 +21,5 @@ Auth::routes(['register'=>false]);
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::resource('category', 'CategoryController');
-Route::resource('food', 'FoodController');
+Route::resource('category', 'CategoryController')->middleware('auth');
+Route::resource('food', 'FoodController')->middleware('auth');
