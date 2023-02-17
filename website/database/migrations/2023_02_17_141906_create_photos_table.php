4eed483aa8e727a290fb48dcaa2f89f6a147b319
Antonis Zalonis <antonis.zalonis@gmail.com>
2023-02-17 15:52:12 +0100

    Ringtones, photos and category models

--- website/database/migrations/2023_02_17_141906_create_photos_table.php
+++ website/database/migrations/2023_02_17_141906_create_photos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCategoriesTable extends Migration
+class CreatePhotosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,13 @@ class CreateCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('photos', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('title');
+            $table->text('description');
+            $table->string('file');
+            $table->string('format')->nullable();
+            $table->string('size')->nullable();
             $table->timestamps();
         });
     }
@@ -27,6 +31,6 @@ class CreateCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('photos');
     }
 }
