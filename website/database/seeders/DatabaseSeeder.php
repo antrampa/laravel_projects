246a274fabab2bf8b184c4b546c939e87beaf3f4
Antonis Zalonis <antonis.zalonis@gmail.com>
2023-02-18 01:51:46 +0100

    Seed category and user

--- website/database/seeders/DatabaseSeeder.php
+++ website/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\Category;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,22 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // User::factory(10)->create();
+        Category::create(['name'=>'Classical']);
+        Category::create(['name'=>'Animals']);
+        Category::create(['name'=>'Funny']);
+        Category::create(['name'=>'SMS']);
+        Category::create(['name'=>'Alarms']);
+        Category::create(['name'=>'Children']);
+        Category::create(['name'=>'Standard']);
+        Category::create(['name'=>'Music']);
+        Category::create(['name'=>'Holiday']);
+        Category::create(['name'=>'Nature']);
+
+        User::create([
+            'name'=>'admin',
+            'email'=>'admin@gmail.com',
+            'password'=>bcrypt('admin12345'),
+            'email_verified_at'=>NOW(),
+        ]);
     }
 }
